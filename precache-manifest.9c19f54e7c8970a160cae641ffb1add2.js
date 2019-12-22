4893743fe916aee2e35f4d873268cc8f2611ae6b
olivier chabot <chabot.oliv@gmail.com>
2019-12-22 18:41:41 +0100

    Updates

---
+++ precache-manifest.9c19f54e7c8970a160cae641ffb1add2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839b0bb7e431f087f21e000f94a6c2c3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e1ec07ab3cde3234c317",
+    "url": "/portfolio/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "bf6f6d3e0c24ad4bc8b4",
+    "url": "/portfolio/static/js/2.a43d60fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.a43d60fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1ec07ab3cde3234c317",
+    "url": "/portfolio/static/js/main.de1e7b5f.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
