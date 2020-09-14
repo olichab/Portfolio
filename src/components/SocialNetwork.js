2fbafc31d30837efbfb48f5f5760391db3cb72db
olivier chabot <chabot.oliv@gmail.com>
2020-09-14 11:46:05 +0200

    add logo instagram

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import { Media } from 'reactstrap';
-import logoLinkedIn from '../assets/images/logos/linkedin.svg';
-import logoGithub from '../assets/images/logos/github.svg';
+import React from "react";
+import { Media } from "reactstrap";
+import logoLinkedIn from "../assets/images/logos/linkedin.svg";
+import logoGithub from "../assets/images/logos/github.svg";
+import logoInstagram from "../assets/images/logos/instagram.svg";
 
-import '../scss/SocialNetwork.scss';
+import "../scss/SocialNetwork.scss";
 
 export default function SocialNetwork() {
   return (
-    <div className='container-social-network'>
+    <div className="container-social-network">
       <Media
         object
         src={logoLinkedIn}
-        alt='logo linkedin'
-        className='logo-linked-in hvr-grow'
+        alt="logo linkedin"
+        className="logo-linked-in hvr-grow"
         onClick={() =>
-          window.open('https://www.linkedin.com/in/olichab', '_blank')
+          window.open("https://www.linkedin.com/in/olichab", "_blank")
         }
       />
       <Media
         object
         src={logoGithub}
-        alt='logo linkedin'
-        className='logo-github hvr-grow'
-        onClick={() => window.open('https://github.com/olichab', '_blank')}
+        alt="logo github"
+        className="logo-github hvr-grow"
+        onClick={() => window.open("https://github.com/olichab", "_blank")}
+      />
+      <Media
+        object
+        src={logoInstagram}
+        alt="logo instagram"
+        className="logo-instagram hvr-grow"
+        onClick={() =>
+          window.open("https://www.instagram.com/devandco/", "_blank")
+        }
       />
     </div>
   );
