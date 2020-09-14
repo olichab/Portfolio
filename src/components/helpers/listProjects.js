d317361cf75ad5e1245348651b7c5b814ca87a9f
olivier chabot <chabot.oliv@gmail.com>
2020-09-14 11:33:21 +0200

    refacto list projects

---
+++ src/components/helpers/listProjects.js
@@ -0,0 +1,64 @@
+const listProjects = [
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/Web/Thumbnail_BriefOnline.jpg"),
+    title: "Brief Online",
+    location: "Altavia Connect",
+    path: "/brief-online",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/Web/Thumbnail_Mooviz.jpg"),
+    title: "Mooviz",
+    location: "Projet personnel",
+    path: "/mooviz",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/Web/Thumbnail_HelloWorld.jpg"),
+    title: "Hello world",
+    location: "Wild Code School",
+    path: "/hello-world",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/3D/Thumbnail_Bullit.jpg"),
+    title: "Viparis La Plaza",
+    location: "Bullit studio",
+    path: "/viparis-la-plaza",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/3D/Thumbnail_LaDefenseArena.jpg"),
+    title: "La Défense Arena",
+    location: "Pacifa Decision",
+    path: "/la-defense-arena",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/3D/Thumbnail_Archi.jpg"),
+    title: "Architecture 3D",
+    location: "Projets personnels",
+    path: "/architecture-3d",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/3D/Thumbnail_PacifaBasket.jpg"),
+    title: "Vues 3D pour billeteries",
+    location: "Pacifa Decision",
+    path: "/billeterie-3D",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/2D/Thumbnail_Uvsq.jpg"),
+    title: "Maquettes et illustrations",
+    location: "UVSQ",
+    path: "/maquettes-et-illustrations",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/3D/Thumbnail_HellsKitchen.jpg"),
+    title: "Hell's Kitchen",
+    location: "Projet personnel",
+    path: "/hells-kitchen",
+  },
+  {
+    urlThumbnail: require("../../assets/images/projets/Thumbnails/2D/Thumbnail_BreakingBad.jpg"),
+    title: "Heisenberg",
+    location: "Projet personnel",
+    path: "/heisenberg",
+  },
+];
+
+export default listProjects;
