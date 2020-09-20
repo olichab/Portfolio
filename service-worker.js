3160a2cc2a23a56967d6a859d6a4543a03eed45c
olivier chabot <chabot.oliv@gmail.com>
2020-09-20 12:23:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f74b6d2198dc06b40568113e757822b8.js"
+  "/precache-manifest.9b2ceb4840d7c71cb6b90359c66be86a.js"
 );
 
 self.addEventListener('message', (event) => {
